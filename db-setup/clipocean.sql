9aeec25d8d8c9c2cd82470c5ca3e4ef989bede75
Eric Wu <eric@havenlife.com>
2024-02-11 08:22:10 -0500

    add UUID support

--- db-setup/clipocean.sql
+++ db-setup/clipocean.sql
@@ -1,6 +1,8 @@
-
+-- Create a new database 
 CREATE DATABASE "clipocean";
-
+-- Create a new user with password 
 CREATE ROLE clipocean WITH LOGIN PASSWORD 'clipocean';
-
+-- Grant privileges to the new user
 GRANT ALL PRIVILEGES ON DATABASE clipocean TO clipocean;
+-- enable support for UUIDs
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
